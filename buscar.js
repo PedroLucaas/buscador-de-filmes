d4467cafc0ed158ba1ffa8c7f1ec3d8228feb8d1
Pedro Lucas <pedro09031@gmail.com>
2022-05-01 20:25:13 +0100

    resolved bug

--- buscar.js
+++ buscar.js
@@ -1,21 +1,28 @@
 const API_KEY = "e4f6a4c9242a2ae28d81ed446acf4d41";
-var nameSearch = document.getElementById("title").value;
-const films = [
-  {
-    name: "documentaries",
-    title: "Documentários",
-    path: `/search/movie?api_key=${API_KEY}&language=pt-BR&query=${nameSearch}`,
-  },
-];
+var submit = document.querySelector("#input");
+let nameSearch;
+var movieData = [];
 
 const getMovies = async (path) => {
   try {
     let url = `https://api.themoviedb.org/3${path}`;
     const response = await fetch(url);
-    return await response.json();
+    return response.json();
   } catch (error) {
     console.log("error getMovies: ", error);
   }
 };
 
-console.log(getMovies(films[0].path)); // eslint-disable-line no-console
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  nameSearch = document.querySelector("#title").value;
+  const movies = [
+    {
+      title: "Documentários",
+      path: `/search/movie?api_key=${API_KEY}&language=en-US&query=${nameSearch}`,
+    },
+  ];
+});
+
+
+
