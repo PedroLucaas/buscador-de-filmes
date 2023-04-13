e8ae948fbb2ed91a09c97e6e7e6a6cfc9216d61d
Pedro Alves <pedro09031@icloud.com>
2023-04-14 00:11:45 +0100

    fix all bugs

--- buscar.js
+++ buscar.js
@@ -1,7 +1,9 @@
 const API_KEY = "e4f6a4c9242a2ae28d81ed446acf4d41";
 var submit = document.querySelector("#input");
 let nameSearch;
-var movieData = [];
+var movieNames = [];
+
+var list = document.getElementById("myList");
 
 const getMovies = async (path) => {
   try {
@@ -11,18 +13,44 @@ const getMovies = async (path) => {
   } catch (error) {
     console.log("error getMovies: ", error);
   }
+  return response.json();
 };
+
+function createElements(id, src, txt) {
+  const div = document.createElement('div');
+  div.style = "background-color: transparent; border-radius: 6%; margin: 5px; border: 2px solid #f4b41a; max-width: 300px;  flex-wrap: wrap;";
+
+  const img = document.createElement('img');
+  img.src = src;
+  img.style = "max-width: 250px;"
+
+  const text = document.createElement('p');
+  text.innerText = "|" + txt + "|";
+  text.style = "color: #fff; font-size: 30px;"
+
+  div.appendChild(img);
+  div.appendChild(text);
+  list.appendChild(div);
+
+}
+
+const clearResult = () => list.innerHTML = "";
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   nameSearch = document.querySelector("#title").value;
-  const movies = [
-    {
-      title: "Documentários",
-      path: `/search/movie?api_key=${API_KEY}&language=en-US&query=${nameSearch}`,
-    },
-  ];
+  const path = `/search/movie?api_key=${API_KEY}&language=en-US&query=${nameSearch}`;
+  const getter = getMovies(path).then((res) => {
+    clearResult();
+    for (let i = 0; i < 20; i++){
+      let pathImage = res.results[i].poster_path;
+      if (pathImage != null)
+        var urlImage = `https://image.tmdb.org/t/p/w500${pathImage}`;
+      let text = res.results[i].title;
+      createElements(i, urlImage, text);
+    }
+  });
+  
+
 });
-
-
 
